e1a0de145aeecdc47a4ea2503c225eddc4c6e5d7
IOYX <seoyeon2julie@naver.com>
2025-08-06 16:39:22 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
-//main.dart
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'pages/splash_page.dart';
 import 'pages/start_page.dart';
 import 'pages/main_page.dart';
-import 'pages/search_page.dart'; // 올바른 경로와 이름으로 수정
+import 'pages/search_page.dart';
 import 'pages/settings_page.dart';
-import 'pages/favorite_items_page.dart';
+import 'pages/favorite_notices_page.dart';
 import 'pages/hidden_items_page.dart';
+import 'pages/year_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  tz.initializeTimeZones();
-
-  // dotenv 먼저 로드
   await dotenv.load(fileName: ".env");
-
-  // Firebase 초기화 추가
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(const MyApp());
+  // await Firebase.initializeApp();
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
@@ -38,9 +32,9 @@ class MyApp extends StatelessWidget {
         '/main_page': (context) => const MainPage(),
         '/search': (context) => const SearchPage(),
         '/settings': (context) => const SettingsPage(),
-        '/favorite': (context) => const FavoriteItemsPage(),
-        '/hidden': (context) => const HiddenItemsPage(),
-        '/filter': (context) => const MainPage(), // 또는 별도 페이지
+        '/favorite': (context) => const FavoriteNoticesPage(),
+        '/hidden': (context) => const HiddenItemsPage(),        
+        '/year_page': (context) => const YearCalendarPage(),
       },
     );
   }
